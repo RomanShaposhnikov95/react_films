e9b81b01789a34fa850f4713c5eb410eaab3f022
Roman Shaposhnikov <romanshaposhnikov95@gmail.com>
2021-12-11 04:24:08 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Route, Routes} from "react-router-dom";
 import {lazy} from "react";
+import HomePage from "./components/HomePage/HomePage";
+import {NotFound} from "./components/Pages/NotFound";
+import AllFilms from "./components/AllFIlms/AllFilms";
+import FilmInfo from "./components/FilmInfo/FilmInfo";
 
-const AllFilms = lazy(() => import("./components/AllFIlms/AllFilms"));
-const FilmInfo = lazy(() => import("./components/FilmInfo/FilmInfo"));
-const NotFound = lazy(() => import("./components/Pages/NotFound"));
-const HomePage = lazy(() => import("./components/HomePage/HomePage"));
+
+// const AllFilms = lazy(() => import("./components/AllFIlms/AllFilms"));
+// const FilmInfo = lazy(() => import("./components/FilmInfo/FilmInfo"));
+// const NotFound = lazy(() => import("./components/Pages/NotFound"));
+// const HomePage = lazy(() => import("./components/HomePage/HomePage"));
 
 
 function App() {
